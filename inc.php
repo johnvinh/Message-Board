a2361398d63cddd90389830a9a05a81c054f1a47
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-05 19:38:17 -0500

    added db details and messages should now be displayed

---
+++ inc.php
@@ -0,0 +1,6 @@
+<?php
+// Database details
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'simplemessageboard');
+define('DB_USER', 'messageboarduser');
+define('DB_PASS', 'RQvszQgBpOTU');
--- index.php
+++ index.php
@@ -16,6 +16,49 @@
             <label for="message">Your Message</label><input type="text" id="message" name="message">
             <input type="submit">
         </form>
+        <table id="posts">
+            <tr>
+                <th>Date</th>
+                <th>User</th>
+                <th>Message</th>
+            </tr>
+            <?php
+            require('inc.php');
+
+            $host = DB_HOST;
+            $user = DB_USER;
+            $pass = DB_PASS;
+            $db = DB_NAME;
+            $charset = 'utf8mb4';
+
+            // Establish database connection
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+
+            try {
+                $pdo = new PDO($dsn, $user, $pass, $options);
+            } catch (PDOException $e) {
+                throw new PDOException($e->getMessage(), (int)$e->getCode());
+            }
+
+            // Displaying existing messages
+            $stmt = $pdo->query('SELECT * FROM posts');
+            while ($row = $stmt->fetch()) {
+                echo '<tr>';
+                echo '<td>' . $row['time'] . '</td>';
+                echo '<td>' . $row['user'] . '</td>';
+                echo '<td>' . $row['message'] . '</td>';
+                echo '</tr>';
+            }
+
+            // Close database connection
+            $pdo = null;
+            ?>
+        </table>
     </main>
 </div>
 </body>
