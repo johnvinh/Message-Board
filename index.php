5c66d9a695b7681a90aee7dc4fe656e8d8491103
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-06 20:18:17 -0500

    cleaned code a bit by adding a file for establishing a db connection

--- index.php
+++ index.php
@@ -24,27 +24,8 @@
                 <th>Message</th>
             </tr>
             <?php
-            require('inc.php');
-
-            $host = DB_HOST;
-            $user = DB_USER;
-            $pass = DB_PASS;
-            $db = DB_NAME;
-            $charset = 'utf8mb4';
-
-            // Establish database connection
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-            $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false
-            ];
-
-            try {
-                $pdo = new PDO($dsn, $user, $pass, $options);
-            } catch (PDOException $e) {
-                throw new PDOException($e->getMessage(), (int)$e->getCode());
-            }
+            $pdo = null;
+            require('dbconnection.php');
 
             // Displaying existing messages
             $stmt = $pdo->query('SELECT * FROM posts ORDER BY time DESC');
--- postmessage.php
+++ postmessage.php
@@ -1,25 +1,6 @@
 <?php
-require('inc.php');
-
-$host = DB_HOST;
-$user = DB_USER;
-$pass = DB_PASS;
-$db = DB_NAME;
-$charset = 'utf8mb4';
-
-// Establish database connection
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+$pdo = null;
+require('dbconnection.php');
 
 if (!isset($_POST))
     exit();
